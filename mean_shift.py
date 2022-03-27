f0e5b585d31ac36c0f634aa6d53392d5f4da64e4
Blarc <jakob.malezic@gmail.com>
2022-03-27 16:34:53 +0200

    [feat] Add parameter comparisons and plots.

--- mean_shift.py
+++ mean_shift.py
@@ -11,7 +11,7 @@ def create_coordinates_kernels(size):
     width = int(size[0])
     if width % 2 == 0:
         width -= 1
-    
+
     height = int(size[1])
     if height % 2 == 0:
         height -= 1
@@ -22,7 +22,7 @@ def create_coordinates_kernels(size):
     half_width = width // 2
     for index, value in enumerate(range(-half_width, half_width + 1)):
         kernel_x[:, index] = value
-    
+
     half_height = height // 2
     for index, value in enumerate(range(-half_height, half_height + 1)):
         kernel_y[index, :] = value
@@ -33,15 +33,17 @@ def create_coordinates_kernels(size):
 def mean_shift(image, patch_position, kernel_size_, epsilon):
     coords_x, coords_y = create_coordinates_kernels(kernel_size_)
 
-    x_change = float('inf')
-    y_change = float('inf')
-
+    converged = False
+    iters_ = 0
     steps_ = [patch_position]
-    while abs(x_change) > epsilon or abs(y_change) > epsilon:
+    while not converged:
         patch, _ = get_patch(image, patch_position, kernel_size_)
 
         x_change = np.divide(np.sum(np.multiply(coords_x, patch)), np.sum(patch))
         y_change = np.divide(np.sum(np.multiply(coords_y, patch)), np.sum(patch))
+
+        if abs(x_change) < epsilon and abs(y_change) < epsilon:
+            converged = True
 
         patch_position = patch_position[0] + x_change, patch_position[1] + y_change
 
@@ -49,16 +51,46 @@ def mean_shift(image, patch_position, kernel_size_, epsilon):
         if steps_[-1] != floored_position:
             steps_.append(floored_position)
 
-    return int(math.floor(patch_position[0])), int(math.floor(patch_position[1])), steps_
+        iters_ += 1
+
+    return int(math.floor(patch_position[0])), int(math.floor(patch_position[1])), steps_, iters_
+
+
+def run_example(img):
+    end_x, end_y, steps, iters = mean_shift(img, (80, 80), (5, 5), 0.01)
+    responses = cv2.circle(img * 400, steps[0], radius=0, color=(255, 0, 0), thickness=-1)
+    print(f'{iters} & {steps[-1]} & {img[steps[-1][1], steps[-1][0]]:.5f}')
+    for step in steps[1:]:
+        responses = cv2.circle(responses, step, radius=0, color=(0, 255, 0), thickness=-1)
+    show_image(cv2.resize(responses, (300, 300)), 0, "Mean shift algorithm")
+
+
+def run_parameters_comparison(img):
+    end_x, end_y, steps, iters = mean_shift(img, (30, 60), (3, 3), 0.01)
+    print(f'Kernel size 3x3 & {iters} & {steps[-1]} & {img[steps[-1][1], steps[-1][0]]:.5f} \\\\\n\\hline')
+    end_x, end_y, steps, iters = mean_shift(img, (30, 60), (5, 5), 0.01)
+    print(f'Kernel size 5x5 & {iters} & {steps[-1]} & {img[steps[-1][1], steps[-1][0]]:.5f} \\\\\n\\hline')
+    end_x, end_y, steps, iters = mean_shift(img, (30, 60), (7, 7), 0.01)
+    print(f'Kernel size 7x7 & {iters} & {steps[-1]} & {img[steps[-1][1], steps[-1][0]]:.5f} \\\\\n\\hline')
+    print('\\hhline{|=|=|=|=|}')
+    end_x, end_y, steps, iters = mean_shift(img, (30, 60), (5, 5), 0.01)
+    print(f'Epsilon 0.01 & {iters} & {steps[-1]} & {img[steps[-1][1], steps[-1][0]]:.5f} \\\\\n\\hline')
+    end_x, end_y, steps, iters = mean_shift(img, (30, 60), (5, 5), 0.05)
+    print(f'Epsilon 0.05 & {iters} & {steps[-1]} & {img[steps[-1][1], steps[-1][0]]:.5f} \\\\\n\\hline')
+    end_x, end_y, steps, iters = mean_shift(img, (30, 60), (5, 5), 0.1)
+    print(f'Epsilon 0.1 & {iters} & {steps[-1]} & {img[steps[-1][1], steps[-1][0]]:.5f} \\\\\n\\hline')
+    print('\\hhline{|=|=|=|=|}')
+    end_x, end_y, steps, iters = mean_shift(img, (35, 40), (5, 5), 0.01)
+    print(f'Start (35, 40) & {iters} & {steps[-1]} & {img[steps[-1][1], steps[-1][0]]:.5f} \\\\\n\\hline')
+    end_x, end_y, steps, iters = mean_shift(img, (60, 30), (5, 5), 0.01)
+    print(f'Start (60, 30) & {iters} & {steps[-1]} & {img[steps[-1][1], steps[-1][0]]:.5f} \\\\\n\\hline')
+    end_x, end_y, steps, iters = mean_shift(img, (75, 75), (5, 5), 0.01)
+    print(f'Start (75, 75) & {iters} & {steps[-1]} & {img[steps[-1][1], steps[-1][0]]:.5f} \\\\\n\\hline')
 
 
 if __name__ == '__main__':
-    responses = generate_responses_1() * 600
-    
-    end_x, end_y, steps = mean_shift(responses, (80, 80), (5, 5), 0.01)
-
-    responses = cv2.circle(responses, steps[0], radius=0, color=(255, 0, 0), thickness=-1)
-    for step in steps[1:]:
-        responses = cv2.circle(responses, step, radius=0, color=(0, 255, 0), thickness=-1)
-
-    show_image(cv2.resize(responses, (300, 300)), 0, "Mean shift algorithm")
+    responses = generate_responses_1()
+    print(f'(50, 70): {responses[50][70]:.5f}')
+    print(f'(70, 50): {responses[70][50]:.5f}')
+    # run_parameters_comparison(responses)
+    run_example(responses)
--- run_tracker.py
+++ run_tracker.py
@@ -1,13 +1,13 @@
 import time
 
 import cv2
+import matplotlib.pyplot as plt
 
 from mean_shift_tracker import MSParams, MeanShiftTracker
 from sequence_utils import VOTSequence
 
 # set the path to directory where you have the sequences
-dataset_path = 'vot2016'
-sequence = 'ball1'  # choose the sequence you want to test
+dataset_path = 'vot2014'
 
 # visualization and setup parameters
 win_name = 'Tracking window'
@@ -16,58 +16,137 @@ show_gt = True
 video_delay = 15
 font = cv2.FONT_HERSHEY_PLAIN
 
-# create sequence object
-sequence = VOTSequence(dataset_path, sequence)
-init_frame = 0
-n_failures = 0
-# create parameters and tracker objects
-# parameters = NCCParams()
-# tracker = NCCTracker(parameters)
 
-parameters = MSParams()
-tracker = MeanShiftTracker(parameters)
+def run_tracker(sequence_name, parameters=MSParams(), show_visualization=True):
+    # create sequence object
+    sequence = VOTSequence(dataset_path, sequence_name)
+    init_frame = 0
+    n_failures = 0
+    tracker = MeanShiftTracker(parameters)
+    time_all = 0
+    # initialize visualization window
+    sequence.initialize_window(win_name)
+    # tracking loop - goes over all frames in the video sequence
+    frame_idx = 0
+    while frame_idx < sequence.length():
+        img = cv2.imread(sequence.frame(frame_idx))
+        # initialize or track
+        if frame_idx == init_frame:
+            # initialize tracker (at the beginning of the sequence or after tracking failure)
+            t_ = time.time()
+            tracker.initialize(img, sequence.get_annotation(frame_idx, type='rectangle'))
+            time_all += time.time() - t_
+            predicted_bbox = sequence.get_annotation(frame_idx, type='rectangle')
+        else:
+            # track on current frame - predict bounding box
+            t_ = time.time()
+            predicted_bbox = tracker.track(img)
+            time_all += time.time() - t_
 
-time_all = 0
+        # calculate overlap (needed to determine failure of a tracker)
+        gt_bb = sequence.get_annotation(frame_idx, type='rectangle')
+        o = sequence.overlap(predicted_bbox, gt_bb)
 
-# initialize visualization window
-sequence.initialize_window(win_name)
-# tracking loop - goes over all frames in the video sequence
-frame_idx = 0
-while frame_idx < sequence.length():
-    img = cv2.imread(sequence.frame(frame_idx))
-    # initialize or track
-    if frame_idx == init_frame:
-        # initialize tracker (at the beginning of the sequence or after tracking failure)
-        t_ = time.time()
-        tracker.initialize(img, sequence.get_annotation(frame_idx, type='rectangle'))
-        time_all += time.time() - t_
-        predicted_bbox = sequence.get_annotation(frame_idx, type='rectangle')
-    else:
-        # track on current frame - predict bounding box
-        t_ = time.time()
-        predicted_bbox = tracker.track(img)
-        time_all += time.time() - t_
+        if show_visualization:
+            # draw ground-truth and predicted bounding boxes, frame numbers and show image
+            if show_gt:
+                sequence.draw_region(img, gt_bb, (0, 255, 0), 1)
 
-    # calculate overlap (needed to determine failure of a tracker)
-    gt_bb = sequence.get_annotation(frame_idx, type='rectangle')
-    o = sequence.overlap(predicted_bbox, gt_bb)
+            sequence.draw_region(img, predicted_bbox, (0, 0, 255), 2)
+            sequence.draw_text(img, '%d/%d' % (frame_idx + 1, sequence.length()), (25, 25))
+            sequence.draw_text(img, 'Fails: %d' % n_failures, (25, 55))
+            sequence.show_image(img, video_delay)
 
-    # draw ground-truth and predicted bounding boxes, frame numbers and show image
-    if show_gt:
-        sequence.draw_region(img, gt_bb, (0, 255, 0), 1)
-    sequence.draw_region(img, predicted_bbox, (0, 0, 255), 2)
-    sequence.draw_text(img, '%d/%d' % (frame_idx + 1, sequence.length()), (25, 25))
-    sequence.draw_text(img, 'Fails: %d' % n_failures, (25, 55))
-    sequence.show_image(img, video_delay)
+        if o > 0 or not reinitialize:
+            # increase frame counter by 1
+            frame_idx += 1
+        else:
+            # increase frame counter by 5 and set re-initialization to the next frame
+            frame_idx += 5
+            init_frame = frame_idx
+            n_failures += 1
 
-    if o > 0 or not reinitialize:
-        # increase frame counter by 1
-        frame_idx += 1
-    else:
-        # increase frame counter by 5 and set re-initialization to the next frame
-        frame_idx += 5
-        init_frame = frame_idx
-        n_failures += 1
+    return tracker, sequence, time_all, n_failures
 
-print('Tracking speed: %.1f FPS' % (sequence.length() / time_all))
-print('Tracker failed %d times' % n_failures)
+
+def run_sequences():
+    # choose the sequences you want to test
+    sequences = ['basketball', 'bolt', 'hand2', 'polarbear', 'jogging', 'fernando']
+    # sequences = ['bolt']
+    for sequence_name in sequences:
+        tracker, sequence, time_all, n_failures = run_tracker(sequence_name, show_visualization=True)
+        print(f'{sequence_name} & {sequence.length()} & {n_failures} \\\\\n\\hline')
+
+
+def create_plot(parameter_, failures_, fps_, parameter_name):
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
+    ax1.plot(parameter_, failures_, marker='s', markerfacecolor='none')
+    ax2.plot(parameter_, fps_, marker='s', markerfacecolor='none', color='green')
+    ax1.set(xlabel=parameter_name, ylabel='#failures', title=f'#failures / {parameter_name}')
+    ax2.set(xlabel=parameter_name, ylabel='FPS', title=f'FPS / {parameter_name}')
+    ax1.grid()
+    ax2.grid()
+    
+    plt.tight_layout()
+    plt.savefig(f'plots/{parameter_name}_failures.png', bbox_inches='tight')
+    plt.show()
+
+
+def run_parameters_comparison(seq='bolt'):
+    params = MSParams()
+    bins = [4, 6, 8, 12, 16]
+    failures = []
+    fps = []
+    for bin_ in bins:
+        params.bins = bin_
+        tracker, sequence, time_all, n_failures = run_tracker(seq, params, show_visualization=False)
+        failures.append(n_failures)
+        fps.append(sequence.length() / time_all)
+
+    create_plot(bins, failures, fps, 'bins')
+
+    params = MSParams()
+    alphas = [0, 0.1, 0.2, 0.4, 0.5, 0.6]
+    failures = []
+    fps = []
+    for alpha_ in alphas:
+        params.alpha = alpha_
+        tracker, sequence, time_all, n_failures = run_tracker(seq, params, show_visualization=False)
+        failures.append(n_failures)
+        fps.append(sequence.length() / time_all)
+
+    create_plot(alphas, failures, fps, 'alpha')
+
+    params = MSParams()
+    sigmas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6]
+    failures = []
+    fps = []
+    for sigma_ in sigmas:
+        params.sigma = sigma_
+        tracker, sequence, time_all, n_failures = run_tracker(seq, params, show_visualization=False)
+        failures.append(n_failures)
+        fps.append(sequence.length() / time_all)
+
+    create_plot(sigmas, failures, fps, 'sigma')
+
+    params = MSParams()
+    epsilons = [0.5, 1.0, 1.5, 2.0, 2.5]
+    failures = []
+    fps = []
+    for epsilon_ in epsilons:
+        params.epsilon = epsilon_
+        tracker, sequence, time_all, n_failures = run_tracker(seq, params, show_visualization=False)
+        failures.append(n_failures)
+        fps.append(sequence.length() / time_all)
+
+    create_plot(epsilons, failures, fps, 'epsilon')
+
+
+if __name__ == '__main__':
+    # run_parameters_comparison('bolt')
+    run_sequences()
+
+    # print('Tracking speed: %.1f FPS' % (sequence.length() / time_all))
+    # print('Tracker failed %d times' % n_failures)
+    # print(f'{sequence_name} & {sequence.length()} & {n_failures} \\\\\n\\hline')
+    # print(f'{tracker.parameters.bins} & {sequence.length() / time_all:.1f} & {n_failures} \\\\\n\\hline')
